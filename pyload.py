bcc2169e37dbc3657bda62562dc28ac8af7a45f4
victorsvart <victor.cscience@gmail.com>
2022-09-06 04:08:44 -0300

    Adding Files

---
+++ pyload.py
@@ -0,0 +1,59 @@
+from tkinter import *
+import tkinter as tk
+from pytube import YouTube
+
+
+def yt_download():
+    url = entry0.get()
+    try:
+        yt = YouTube(url)
+        video = yt.streams.get_highest_resolution()
+        video.download("C:/Users/Light/Desktop/coding/snake/youtube/videos")
+        notif.config(fg="green", text="Download Completed!")
+    except Exception as e:
+        print(e)
+        notif.config(
+            fg="red",
+            text=
+            "Video could not be downloaded. Perhaps the link is not copied correctly."
+        )
+
+
+window = tk.Tk()
+window.title("PyLoad")
+window.iconbitmap("./assets/icon.ico")
+
+window.geometry("1000x600")
+window.configure(bg="#212121")
+canvas = Canvas(window,
+                bg="#212121",
+                height=600,
+                width=1000,
+                bd=0,
+                highlightthickness=0,
+                relief="ridge")
+canvas.place(x=0, y=0)
+
+background_img = PhotoImage(file=f"./assets/background.png")
+background = canvas.create_image(224.0, 386.5, image=background_img)
+
+entry0_img = PhotoImage(file=f"./assets/img_textBox0.png")
+entry0_bg = canvas.create_image(501.0, 300.0, image=entry0_img)
+
+entry0 = Entry(bd=0, bg="#000000", fg="white", highlightthickness=0)
+
+entry0.place(x=158.0, y=279, width=686.0, height=40)
+
+img0 = PhotoImage(file=f"./assets/img0.png")
+b0 = Button(image=img0,
+            borderwidth=0,
+            highlightthickness=0,
+            command=yt_download,
+            relief="flat")
+
+b0.place(x=427, y=339, width=146, height=30)
+
+notif = Label(window, font=("Segoe UI", 12))
+notif.grid(column=1, row=3, sticky=tk.S)  # not working properly
+window.resizable(False, False)
+window.mainloop()
